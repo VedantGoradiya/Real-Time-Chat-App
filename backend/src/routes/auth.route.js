d6ba2332d846b5881eae58c65b109e959a3c9588
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-09 22:42:13 -0500

    Feat: Added router for update profile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import { login, signup, logout, updateProfile  } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;

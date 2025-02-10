2b279a0059f011b4bbcc5ba7e230b7aa5097e7cf
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-09 22:42:46 -0500

    Feat: Created message api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,24 @@
-import express from 'express'
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDB } from './lib/db.js'
-import authRoutes from './routes/auth.route.js'
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server running on port:', + PORT)
-    connectDB()
-})
+  console.log("Server running on port:", +PORT);
+  connectDB();
+});

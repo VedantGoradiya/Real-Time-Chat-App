d603bb174e226907726979cd078191f9faa26e99
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-18 15:55:31 -0500

    Feat: Added navbar component and initials pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navbar from "./component/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 
 const App = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div>
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUpPage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={<ProfilePage />}
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
